b76f78f00424069b6fba9590f50b370b08335e29
Sindre Sorhus <sindresorhus@gmail.com>
2012-09-24 22:59:20 +0200

    Many improvements to the task

--- tasks/sass.js
+++ tasks/sass.js
@@ -1,21 +1,35 @@
-module.exports = function( grunt ) {
+module.exports = function(grunt) {
 	'use strict';
 
-	grunt.registerMultiTask( 'sass', 'Compile SASS .scss using node-sass', function() {
+	// TODO: ditch this when grunt v0.4 is released
+	grunt.util = grunt.util || grunt.utils;
+
+	grunt.registerMultiTask('sass', 'Compile SCSS to CSS', function() {
+		var helpers = require('grunt-contrib-lib').init(grunt);
+		var cb = this.async();
 		var sass = require('node-sass');
+		var async = grunt.util.async;
 
-		this.files.forEach(function( el ) {
-			var files = grunt.file.expand( el.src );
+		// TODO: ditch this when grunt v0.4 is released
+		this.files = this.files || helpers.normalizeMultiTaskFiles(this.data, this.target);
+
+		async.forEachSeries(this.files, function(el, cb2) {
+			var files = grunt.file.expand(el.src);
 			var dest = el.dest;
-			var max = grunt.helper( 'concat', files );
+			var max = grunt.helper('concat', files);
 
-			sass.render( max, function( err, css ) {
-				if ( err ) {
-					grunt.fail.fatal( err );
+			sass.render(max, function(err, css) {
+				if (err) {
+					grunt.warn(err);
 				}
-				grunt.file.write( dest, css );
-				grunt.helper( 'min_max_info', css, max );
+
+				grunt.file.write(dest, css);
+				grunt.helper('min_max_info', css, max);
+
+				cb2();
 			});
+		}, function(err) {
+			cb(!err);
 		});
 	});
 };
